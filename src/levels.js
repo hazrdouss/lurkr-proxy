7d92e741a1e448e47410aa759825d83ce55cd008
Bob <hazrdousss@outlook.com>
2025-04-08 19:19:56 +0800

    refactor(levels.js): added error handling, rotate key notification

--- src/levels.js
+++ src/levels.js
@@ -5,6 +5,16 @@ export default {
 	},
 };
 
+function parseJwt (token) {
+	const base64Url = token.split('.')[1];
+	const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+	const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+		return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+	}).join(''));
+
+	return JSON.parse(jsonPayload);
+}
+
 async function getLevelData(url) {
 	const levelDataResponse = await fetch(url, { method: 'GET'})
 	const levelData = await levelDataResponse.json();
@@ -16,6 +26,11 @@ async function getLevelData(url) {
 }
 
 async function handleRequest(token) {
+	const curUnix = Math.round(Date.now() / 1000);
+	if (parseJwt(token).exp - curUnix <= 0) {
+		return new Response("ROTATE TOKEN")
+	}
+
 	const lurkrExportEndpoint = 'https://api.lurkr.gg/v2/levels/1356262077857267904/export';
 	let exportData = null;
 
